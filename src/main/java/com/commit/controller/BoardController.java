cfc9f44516dcd118b8e8ed184e6fc50e7e7d5033
이성길 <istjprogrammer@gmail.com>
2024-01-05 17:23:07 +0900

    성길 : 어노테이션 추가 및 BoardController, BoardService 오류 해결

--- src/main/java/com/commit/controller/BoardController.java
+++ src/main/java/com/commit/controller/BoardController.java
@@ -3,6 +3,11 @@ package com.commit.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.commit.entity.Board;
@@ -19,9 +24,9 @@ import com.commit.service.BoardService;
 
 @RestController
 public class BoardController {
+    @Autowired
 	private final BoardService boardService;
 
-    @Autowired
     public BoardController(BoardService boardService) {
         this.boardService = boardService;
     }
